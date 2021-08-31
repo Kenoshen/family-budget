bd6960f8f427663ef29620e1ab9b206139465f8a
mwingfield <mwingfie@gmail.com>
2021-08-31 08:36:57 -0600

    more log messages to try and figure out why it isn't working

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -12,7 +12,7 @@ const RefillEveryWeek = "week"
 const RefillEveryMonth = "month"
 const RefillEveryYear = "year"
 
-// run this every day at 01:00am
+// run this every day at 01:00am (central)
 export const dailyRefillCheck = functions.pubsub.schedule("0 1 * * *").onRun(async (_) => {
     let now = new Date();
     console.log(`Do daily refill check at ${now.toISOString()} day: ${now.getDay()} date: ${now.getDate()} month: ${now.getMonth()}`)
@@ -27,28 +27,33 @@ export const dailyRefillCheck = functions.pubsub.schedule("0 1 * * *").onRun(asy
             cycles.set(RefillEveryYear, true)
         }
     }
-    console.log(`Cycles: ${cycles}`)
+    console.log(`Cycles: ${JSON.stringify(cycles)}`)
     let updatedEnvelopes:string[] = []
 
     let updateEnvelopeAmount = (snap: firestore.DocumentSnapshot): Promise<any> => {
-        let refillEvery: string = snap.get("refillEvery")
-        if (cycles.get(refillEvery)) {
-            let refillAmount: number = snap.get("refillAmount")
-            let amount: number = snap.get("amount")
-            let newAmount = amount + refillAmount
-            let allowOverfill: boolean = snap.get("allowOverfill")
-            if (newAmount > refillAmount && !allowOverfill) {
-                newAmount = refillAmount
-            }
+        let data = snap.data()
+        if (data) {
+            let refillEvery: string = data.refillEvery
+            if (cycles.get(refillEvery)) {
+                let refillAmount: number = data.refillAmount
+                let amount: number = data.amount
+                let newAmount = amount + refillAmount
+                let allowOverfill: boolean = data.allowOverfill
+                if (newAmount > refillAmount && !allowOverfill) {
+                    newAmount = refillAmount
+                }
 
-            let a = {desc: "refilled", amt: refillAmount, on: new Date().toISOString()};
-            let activity = snap.get("activity")
-            if (!activity) {
-                activity = []
+                let a = {desc: "refilled", amt: refillAmount, on: now.toISOString()};
+                let activity = data.activity
+                if (!activity) {
+                    activity = []
+                }
+                activity.push(a);
+                updatedEnvelopes.push(snap.ref.path)
+                return snap.ref.update({"amount": newAmount, activity})
             }
-            activity.push(a);
-            updatedEnvelopes.push(snap.ref.path)
-            return snap.ref.update({"amount": newAmount, activity})
+        } else {
+            console.log(`Snapshot data was undefined for ${snap.ref.path}`)
         }
         return Promise.resolve();
     };
@@ -62,5 +67,5 @@ export const dailyRefillCheck = functions.pubsub.schedule("0 1 * * *").onRun(asy
     console.log(`Updating a total of ${docs.length} elements`)
     await Promise.all([...docs.map((d) => updateEnvelopeAmount(d))])
     console.log(`Finished updating ${updatedEnvelopes.length} envelopes`)
-    console.log(`Envelopes updated:\n${updatedEnvelopes.join("\n")}`)
+    console.log(`Envelope ids:\n${updatedEnvelopes.join("\n")}`)
 });
